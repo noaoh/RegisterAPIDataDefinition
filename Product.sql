5b61ea26aaeeef5c7040ea547b7cb444787b0d38
Noah Holt <noahryanholt@gmail.com>
2019-03-30 13:50:03 -0500

    Fixed price fields, currently having problem with trigger

--- Product.sql
+++ Product.sql
@@ -7,16 +7,11 @@ CREATE TABLE product (
   lookupcode character varying(32) NOT NULL DEFAULT(''), --Stored in the edu.uark.models.entities:ProductEntity#lookupCode property. See also the named constant defined in edu.uark.models.entities.fieldnames:ProductFieldNames that is used for Java <-> SQL mappings.
   count int NOT NULL DEFAULT(0), --Stored in the edu.uark.models.entities:ProductEntity#count property. See also the named constant defined in edu.uark.models.entities.fieldnames:ProductFieldNames that is used for Java <-> SQL mappings.
   createdon timestamp without time zone NOT NULL DEFAULT now(), --Stored in the edu.uark.dataaccess.entities:BaseEntity#createdOn property. See also the named constant defined in edu.uark.dataaccess.entities:BaseFieldNames that is used for Java <-> SQL mappings.
-  price decimal(12, 2) NOT NULL DEFAULT(0), -- Stored in the edu.uark.edu.models.entities:ProductEntity#price property.  See also the named constant defined in edu.uark.models.entities.fieldNames:ProductFieldNames that is used for Java <-> SQL mappings.
+  price float(25) NOT NULL DEFAULT(0), -- Stored in the edu.uark.edu.models.entities:ProductEntity#price property.  See also the named constant defined in edu.uark.models.entities.fieldNames:ProductFieldNames that is used for Java <-> SQL mappings.
   CONSTRAINT product_pkey PRIMARY KEY (id)
 ) WITH (
   OIDS=FALSE
 );
-
-ALTER TABLE product MODIFY COLUMN
-(
-  count int NOT NULL DEFAULT(0) unsigned
-)
 
 --DROP INDEX ix_product_lookupcode;
 
