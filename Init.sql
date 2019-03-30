12af683576f55865e638e376df1555101f956d1b
Noah Holt <noahryanholt@gmail.com>
2019-03-30 14:05:25 -0500

    Removed the trigger from Init.sql

--- Init.sql
+++ Init.sql
@@ -101,20 +101,3 @@ CREATE TABLE transaction (
 ) WITH (
   OIDS=FALSE
 );
-CREATE TRIGGER product_inventory_update
-	ON TRANSACTION
-	AFTER INSERT
-	AS 
-BEGIN
-	DECLARE 
-	@ProductId numeric,
-	@Quantity numeric
-
-	SELECT @ProductId = INSERTED.Product_Id,
-		@Quantity = INSERTED.quantity
-	FROM INSERTED
-
-	UPDATE Inventory
-		SET QUANTITY = QUANTITY - @Quatity
-		WHERE Product_Id = @ProductId
-END;
